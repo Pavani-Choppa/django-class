5990406360fbac8fe8462ffcb4b6ee92b73b76ed
Pavani-Choppa <pavanicm011@gmail.com>
2026-08-20 19:27:43 +0530

    Worked on Multi model project and usage of forgign key on 2 models

---
+++ MultiModelProject/myapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Department'
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    salary = models.IntegerField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'Employee'
